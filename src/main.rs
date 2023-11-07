33197800f105d95b96fe2810688ac73e8d0109fa
Mashanz <mangatkk@gmail.com>
2023-11-07 20:20:48 +0700

    Get IP

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, App, HttpRequest, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+async fn index(req: HttpRequest) -> impl Responder {
+    let Some(client_ip) = req.peer_addr().map(|addr| addr.ip()) else { todo!() };
+    HttpResponse::Ok().body(client_ip.to_string())
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(index))
+        .bind(("0.0.0.0", 8080))?
+        .run()
+        .await
 }
