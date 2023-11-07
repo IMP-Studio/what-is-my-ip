3d5ebad0b4a207dc4d51d30d9b100b0410c11162
Mashanz <mangatkk@gmail.com>
2023-11-07 21:16:41 +0700

    fix: remote address

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,8 @@ use actix_web::{get, App, HttpRequest, HttpResponse, HttpServer, Responder};
 
 #[get("/")]
 async fn index(req: HttpRequest) -> impl Responder {
-    let Some(client_ip) = req.peer_addr().map(|addr| addr.ip()) else { todo!() };
-    HttpResponse::Ok().body(client_ip.to_string())
+    let conn_info = req.connection_info();
+    HttpResponse::Ok().body(conn_info.realip_remote_addr().unwrap().to_string())
 }
 
 #[actix_web::main]
