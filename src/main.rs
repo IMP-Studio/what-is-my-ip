0971e7a6ad3e8a41b94ff7b89de95fb43900be6c
Mashanz <mangatkk@gmail.com>
2023-11-08 11:41:41 +0700

    perf: fix unwrap

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,11 @@ use actix_web::{get, App, HttpRequest, HttpResponse, HttpServer, Responder};
 #[get("/")]
 async fn index(req: HttpRequest) -> impl Responder {
     let conn_info = req.connection_info();
-    HttpResponse::Ok().body(conn_info.realip_remote_addr().unwrap().to_string())
+    if let Some(realip_remote_addr) = &conn_info.realip_remote_addr() {
+        HttpResponse::Ok().body(realip_remote_addr.to_string())
+    } else {
+        HttpResponse::Ok().body("")
+    }
 }
 
 #[actix_web::main]
