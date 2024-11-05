b8cdf2c3405b2af87ed79998d2cc50769c91f8d7
Mashanz <mangatkk@gmail.com>
2024-11-05 19:43:05 +0700

    fix: 3 type

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use actix_cors::Cors;
 use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use serde_json::Serializer;
 
 async fn index(req: HttpRequest) -> impl Responder {
     let conn_info = req.connection_info();
@@ -10,11 +11,30 @@ async fn index(req: HttpRequest) -> impl Responder {
     HttpResponse::Ok().body(response)
 }
 
+#[derive(Serializer)]
+struct IP {
+    a: string,
+    b: string,
+    c: string,
+}
+
+async fn all(req: HttpRequest) -> impl Responder {
+    let conn_info = req.connection_info();
+    let mut response: String = "".to_string();
+    let ip = IP {
+        a: conn_info.host(),
+        b: conn_info.realip_remote_addr(),
+        c: conn_info.peer_addr()
+    };
+    HttpResponse::Ok().json(ip)
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .wrap(Cors::permissive())
+            .route("/all", web::get().to(all))
             .default_service(web::get().to(index))
     })
     .bind(("0.0.0.0", 8080))?
